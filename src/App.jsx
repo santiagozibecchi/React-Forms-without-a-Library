b5eea4c2cad69951d9336d5acdeac91283e2f0a1
Santiago Zibecchi <santiago.zibecchi@gmail.com>
2022-09-19 19:25:12 -0300

    not re-rendered with new FormData() and Object.fromEntries()

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
+import { useRef, useState } from "react";
 import "./App.css";
+import FormInput from "./components/FormInput";
 
 function App() {
+   //    const [username, setUsername] = useState("");
+
+   console.log("re-rendered");
+
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      const data = new FormData(e.target);
+      console.log(Object.fromEntries(data.entries()));
+   };
+
    return (
       <div className="app">
-         <form ></form>
+         <form onSubmit={handleSubmit}>
+            <FormInput name="Username" placeholder="Username" />
+            <FormInput name="Email" placeholder="Email" />
+            <FormInput name="Full Name" placeholder="Full Name" />
+            <FormInput name="Other" placeholder="Other" />
+            <button>Submit</button>
+         </form>
       </div>
    );
 }
---
+++ src/components/FormInput.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./FormInput.css";
+
+const FormInput = (props) => {
+   return (
+      <div className="formInput">
+         {/* <label>Username</label> */}
+         <input name={props.name} placeholder={props.placeholder} />
+      </div>
+   );
+};
+
+export default FormInput;
